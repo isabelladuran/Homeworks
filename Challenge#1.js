0d3fd228628dfdecc806e129e7e6a1d9f42a3375
isabelladuran <110541100+isabelladuran@users.noreply.github.com>
2023-08-22 21:05:37 -0500

    Challenge función flecha

---
+++ Challenge#1.js
@@ -0,0 +1,20 @@
+// ------------------Challenge #1------------------
+// Isabella Durán Nagles Cód: 2210296
+
+const regular = function (n){
+    if (n % 2 === 0) {
+        console.log(n, 'es par')
+    } else {
+        console.log(n, 'es impar')
+    }
+}
+regular(5)
+
+const flecha = (m) => {
+    if (m % 2 === 0) {
+        console.log(m, 'es par')
+    } else {
+        console.log(m, 'es impar')
+    }
+}
+flecha(100)
